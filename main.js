a40e4d2c86dee1648879e35dc94a2f95f7f461f4
Philipp Stucki <philipp@qwert.ch>
2014-02-17 18:54:38 +0100

    adds working marcher and terrain

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 // make jslint stop after the first error so we can do all sort of nasty tricks
 /*jslint passfail: true*/
 
+a = document.getElementById('c');
+c = a.getContext('2d');
+// declare global vars to enable shortening by google closure compiler
+var pixelSize = 5,
+    PI = Math.PI,
+    w=a.width,
+    vw=w/pixelSize|0,
+    h=a.height,
+    vh=h/pixelSize|0,
+    fps,
+    t,
+    st,
+    run=1;
+
 // shim layer with setTimeout fallback
 window.requestAnimFrame = (function(){
   return  window.requestAnimationFrame       ||
@@ -11,38 +25,49 @@ window.requestAnimFrame = (function(){
           };
 })();
 
-a = document.getElementById('c');
-c = a.getContext('2d');
 c.fillStyle = 'black';
 
-// declare global vars to enable shortening by google closure compiler
-var pixelSize = 10,
-    PI = Math.PI,
-    w=a.width,
-    h=a.height,
-    fps,
-    t,
-    st,
-    run=1;
 
 t = fps = 0;
 st = new Date().getTime();
 
-function pp(x, y) {
+function pp(x, y, b) {
+    c.fillStyle = 'rgb(0,'+(b==-1?0:b)+',0)';
     c.fillRect(x*pixelSize, y*pixelSize, pixelSize, pixelSize);
 }
 
 render = function(t, fps) {
-    for (y = 0; y < h/pixelSize|0; y++) {
-        for (x = 0; x < w/pixelSize|0; x++) {
-            var b = t%255;
-            c.fillStyle = 'rgb('+0+','+b+','+0+')';
-            pp(x, 20+Math.sin(t/5+PI*x/20)*20);
+    var f=1,imin=0.1;imax=25,di=0.2;
+    var x,y,d,b;
+    var o={x:0,y:4+1*Math.sin(t*.1)+0.2*Math.sin(t*0.4),z:-15+t/10}
+
+    for (y = 0; y < vh; y+=1) {
+        for (x = 0; x < vw; x+=1) {
+            d={x:x/vw-0.5,y:y/vh-0.9,z:f};
+            b=-1;
+
+
+            for (i=imin; i<imax; i+=di) {
+                var p = {
+                    x:o.x+i*d.x,
+                    y:o.y+i*d.y,
+                    z:o.z+i*d.z,
+                }
+                var fy = 5*Math.sin(0.1*p.x)*Math.sin(0.1*p.z);
+                fy += 0.2*Math.sin(5*p.x)*Math.sin(4*p.z);
+
+                if (p.y < fy) {
+                    b=(fy+1)/2*250|0;
+                    break;
+                }
+            }
+
+            pp(x,vh-y,b,p.z-o.z);
         }
     }
 }
 
-// add event listener to bind function keys
+// add event listener to bind fun2tion keys
 document.addEventListener('keydown', function(e) {
     run = e.keyCode==32 ? run*-1 : run*1;
 });
@@ -53,5 +78,6 @@ document.addEventListener('keydown', function(e) {
     if (run==1) {
         render(t++, fps);
         fps = Math.round(10*(t*1E3/(new Date().getTime() - st)))/10;
+        t%10==0 && console.log(fps);
     }
 })();
