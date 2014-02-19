001f13b6dc730eea526b400e1b4991eb9ee02d27
Philipp Stucki <philipp@qwert.ch>
2014-02-19 08:42:43 +0100

    adds pregenerated terrain

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 a = document.getElementById('c');
 c = a.getContext('2d');
 // declare global vars to enable shortening by google closure compiler
-var pixelSize = 4,
+var pixelSize = 8,
     PI = Math.PI,
     w=a.width,
     vw=w/pixelSize|0,
@@ -13,7 +13,13 @@ var pixelSize = 4,
     fps,
     t,
     st,
-    run=1;
+    run=1,
+    hm=[],
+    ts=3000,
+    pxmax=0,
+    pymax=0,
+    pzmax=0
+    ;
 
 // shim layer with setTimeout fallback
 window.requestAnimFrame = (function(){
@@ -31,25 +37,31 @@ c.fillStyle = 'black';
 t = fps = 0;
 st = new Date().getTime();
 
+for (ty=0;ty<ts;ty++) {
+    for (tx=0;tx<ts;tx++) {
+        hm[ty*ts+tx]=0.5+Math.sin(ty/8)*Math.sin(tx/8)/2;
+    }
+}
+
 function pp(x, y, h, d) {
-    var fg=35;
+    var fg=25;
     b=d>fg?(Math.exp(-(d-fg)*fg*1e-2)*h)|0:h;
     c.fillStyle = 'rgb(0,'+(h==-1?0:b)+',0)';
     c.fillRect(x*pixelSize, y*pixelSize, pixelSize, pixelSize);
 }
 
 render = function(t, fps) {
-    var f=1,imin=0.1;imax=40,di=0.2;
-    var x,y,d,b,fy=0,qd=8,qs=4,qh;
+    var f=Math.sin(t/30)/2+0.5,f=1,imin=0.1;imax=40,di=0.2;
+    var x,y,d,b,fy=0;
     var o={
-        x:8,
-        y:4+0.5*Math.sin(t/4)+2*Math.sin(t/100),
-        z:-2+t/10
+        x:150,
+        y:8,
+        z:250+t/2
     }
 
     for (y = 0; y < vh; y+=1) {
         for (x = 0; x < vw; x+=1) {
-            d={x:x/vw-(1+Math.sin(t/40))*0.5,y:y/vh-0.8,z:f};
+            d={x:x/vw-0.5,y:y/vh-0.8,z:f};
             b=-1;
 
 
@@ -60,17 +72,14 @@ render = function(t, fps) {
                     z:o.z+i*d.z,
                 }
 
-                ct = t/30;
-                fy = Math.sin(1.5*p.x/2)
-                * Math.cos(1.5*p.z/2)
-                * Math.sin(1+p.x/3)
-                * Math.sin(0.5+p.z/6);
+                pxmax=Math.max(pxmax,p.x);
+                pymax=Math.max(pymax,p.y);
+                pzmax=Math.max(pzmax,p.z);
 
-                fy = (fy+1)/2;
-
+                fy = hm[((p.z*10)%ts*ts+(p.x*10)%ts)|0];
 
                 if (p.y < fy) {
-                    b=fy*200|0;
+                    b=fy*250|0;
                     break;
                 }
             }
@@ -86,7 +95,7 @@ document.addEventListener('keydown', function(e) {
 });
 
 (function loop() {
-    //requestAnimFrame(loop);
+    requestAnimFrame(loop);
     // only render when running
     if (run==1) {
         render(t++, fps);
