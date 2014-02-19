7f0b0c760270ee13f953c35f0bf40082e43d5f02
Philipp Stucki <philipp@qwert.ch>
2014-02-19 22:32:47 +0100

    adds terrain plotter, tg still broken

--- main.js
+++ main.js
@@ -3,8 +3,12 @@
 
 a = document.getElementById('c');
 c = a.getContext('2d');
+
+hme = document.getElementById('hm');
+hmc = hme.getContext('2d');
+
 // declare global vars to enable shortening by google closure compiler
-var pixelSize = 6,
+var pixelSize = 8,
     PI = Math.PI,
     w=a.width,
     vw=w/pixelSize|0,
@@ -14,7 +18,7 @@ var pixelSize = 6,
     t,
     st,
     run=1,
-    ts=8,
+    ts=8+1,
     hm=[],
     mr=Math.random,
     ms=Math.sin
@@ -35,63 +39,65 @@ c.fillStyle = 'black';
 t = fps = 0;
 st = new Date().getTime();
 
-// sine bumps
-//for (ty=0;ty<ts;ty++) {
-    //for (tx=0;tx<ts;tx++) {
-        //hm[ty*ts+tx]=0.5+Math.sin(ty/8)*Math.sin(tx/8)/2;
-    //}
-//}
-
 // seed corners
-hm[0]=hm[ts-1]=hm[ts*ts-ts]=hm[ts*ts-1]=mr();
+hm[0]=hm[ts-1]=hm[ts*ts-ts]=hm[ts*ts-1]=0.5;
 
 function terrainstep(x, y, size, i) {
     var bi = y*ts+x,
-        lo = i*size,
+        ofs = (Math.pow(2,i)-1)*size,
         tl=bi,
         tr=bi+size-1,
-        bl=bi+lo+size*(size-1),
-        br=bi+lo+size*size-1,
+        bl=bi+size*(size-1)+ofs*(size-1),
+        br=bl+size-1,
         t=tl+size/2-1,
         b=bl+size/2-1,
-        cl=tl+size*(size-size/2-1),
+        cl=tl+(size+ofs)*(size-size/2-1),
         cr=cl+size-1,
         cm=cl+size/2-1,
-        displ=function(){return (mr()-0.5)/2;}
+        norm=function(v){v+=(mr()-0.5)/(Math.pow(2,i));return v>1?1:v<0?0:v;}
 
-    hm[t]=hm[t+1]=(hm[tl]+hm[tr])/2+displ(); // top
-    hm[b]=hm[b+1]=(hm[bl]+hm[br])/2+displ(); // bottom
-
-    //hm[cl]=hm[cl+8]=(hm[tl]+hm[bl])/2+displ(); //center left
-    //hm[cr]=hm[cr+8]=(hm[tr]+hm[br])/2+displ(); //center right
-    //hm[cm]=hm[cm+1]=hm[cm+8]=hm[cm+9] //center
-        //=(hm[tl]+hm[tr]+hm[bl]+hm[br])/4+displ();
+    hm[cm]=hm[cm+1]=hm[cm+ts]=hm[cm+ts+1]=norm((hm[tl]+hm[tr]+hm[bl]+hm[br])/4); // center
+    hm[t]=hm[t+1]=norm((hm[tl]+hm[tr])/2); // top
+    hm[b]=hm[b+1]=norm((hm[bl]+hm[br])/2); // bottom
+    hm[cl]=hm[cl+ts]=norm((hm[tl]+hm[bl])/2); //center left
+    hm[cr]=hm[cr+ts]=norm((hm[tr]+hm[br])/2); //center right
 
     if (size>4) {
-        terrainstep(0,0,size/2,i+1);
-        terrainstep(size/2,0,sixe/2,i+1);
-        terrainstep(0,size/2,size/2,i+1);
-        terrainstep(size/2,size/2,size/2,i+1);
+        terrainstep(x,y,size/2,i+1);
+        terrainstep(x+size/2,y,size/2,i+1);
+        terrainstep(x,y+size/2,size/2,i+1);
+        terrainstep(x+size/2,y+size/2,size/2,i+1);
     }
 }
 terrainstep(0,0,ts,0);
+plotHm();
 
+function plotHm() {
+    var ps=40-Math.log(ts)/Math.LN2;
+    for(y=0;y<ts;y++) {
+        for(x=0;x<ts;x++) {
+            pp(x,y,200,2);
+            hmc.fillStyle = 'rgb(0,'+(hm[y*ts+x]*250|0)+',0)';
+            hmc.fillRect(x*ps, y*ps, ps, ps);
+        }
+    }
+}
 
 
 function pp(x, y, h, d) {
-    var fg=75;
+    var fg=25;
     b=d>fg?(Math.exp(-(d-fg)*fg*1e-2)*h)|0:h;
     c.fillStyle = 'rgb(0,'+(h==-1?0:b)+',0)';
     c.fillRect(x*pixelSize, y*pixelSize, pixelSize, pixelSize);
 }
 
 render = function(t, fps) {
-    var f=Math.sin(t/30)/2+0.5,f=1,imin=0.1;imax=40,di=0.2;
+    var f=Math.sin(t/30)/2+0.5,f=1,imin=0.1;imax=40,di=0.05;
     var x,y,d,b,fy=0;
     var o={
-        x:250,
+        x:10+t,
         y:8,
-        z:250
+        z:250+t
     }
 
     for (y = 0; y < vh; y+=1) {
@@ -125,7 +131,7 @@ document.addEventListener('keydown', function(e) {
 });
 
 (function loop() {
-    requestAnimFrame(loop);
+    //requestAnimFrame(loop);
     // only render when running
     if (run==1) {
         render(t++, fps);
